a1195b4cfa5f458b04c14dc9c1ac1f58268a733e
Amine A <amine-98@live.fr>
2020-04-05 17:57:30 +0200

    Migrated all Trakt requests to axios and improved error handling, headers sent and return values.

--- functions/trakt/index.js
+++ functions/trakt/index.js
@@ -1,7 +1,7 @@
-//TODO : Move everything to axios
+//Todo : Regarding API calls, it may be relevant to fetch the popular movies once in a while to "cache" them in some storage somewhere ?
+const axiosModule = require("axios");
 
-//Todo : Examine if request-promise-native would be better for Node.js v8.
-const rp = require("request-promise");
+const TraktCallErrorMessage = "An issue occurred interacting with the TMDB API";
 
 class TraktApi {
     constructor(clientId, endpoint) {
@@ -10,27 +10,35 @@ class TraktApi {
         if (endpoint) {
             this.APIEndpoint = endpoint;
         }
-    }
-
-    getUserSettings(token) {
-        let settingsOptions = {
-            method: 'GET',
-            uri: `${this.APIEndpoint}/users/settings`,
+        this.axios = axiosModule.create({
+            baseURL: this.APIEndpoint,
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
                 'trakt-api-version': '2',
                 'trakt-api-key': `${this.clientId}`
-            }, json: true,
-            resolveWithFullResponse: true
-        };
-        return rp(settingsOptions);
+            },
+        });
+    }
+
+    /**
+     * @param token : string auth access token
+     */
+    async getUserSettings(token) {
+        try {
+            const res = await this.axios({
+                method: 'GET',
+                url: `/users/settings`,
+                headers: {'Authorization': `Bearer ${token}`}
+            });
+            return res.data;
+        } catch (err) {
+            console.error(err.response.status);
+            console.error(err.config);
+            throw this.TraktCallErrorMessage;
+        }
     };
 
-//Todo test
     /**
-     *
-     * @param token auth Access token
      * @param textQuery : string Text query to search for
      * @param years : number 4 digit year, or range of years
      * @param page : number result page to get
@@ -39,97 +47,91 @@ class TraktApi {
      * @param extended : boolean whether to get extended results or not.
      * @param limit : number number of items per page.
      */
-    getSearchResults(token, {textQuery, year = ""}, page = 1, types = ["show", "movie"], extended = false, limit = 10) {
-        if (types === "") {//
-            types = ["show", "movie"];
-        }
-        let searchOptions = {
-            method: 'GET',
-            uri: `${this.APIEndpoint}/search/${types}?page=${page}&limit=${limit}${extended === true ? `&extended=full` : ''}${year !== "" ? `&years=${year}` : ''}&query=${encodeURIComponent(textQuery)}`, //-> search/type1,type2,type3?..
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-                'trakt-api-version': '2',
-                'trakt-api-key': `${this.clientId}`
-            }, json: true,
-            resolveWithFullResponse: true
+    async getSearchResults({textQuery, year = ""}, page = 1, types = ["show", "movie"], extended = false, limit = 10) {
+        (types === "") ? types = ["show", "movie"] : '';
+        let params = {
+            page: page,
+            limit: limit,
+            query: textQuery
         };
-        return rp(searchOptions);
+        (extended) ? params.extended = 'full' : '';
+        (year !== "") ? params.years = year : '';
 
-        /*    return traktApi.search(conv.user.access.token, "Who")
-            .then(response => {
-                console.log(response.headers);
-                console.log(response.body);
-                return true;
-            })
-            .catch(err => {
-                console.log(err);
-                return false;
-            });*/
+        try {
+            const res = await this.axios({
+                method: 'GET',
+                url: `/search/${types}`, //-> search/type1,type2,type3?..
+                params
+            });
+            return res.data;
+        } catch (err) {
+            console.error(err.response.status);
+            console.error(err.config);
+            throw this.TraktCallErrorMessage;
+        }
     };
 
     /**
-     * @param token auth Access token
-     * @param id : string Text query to search for
-     * @param id_type="trakt" : string : "","",""  : string Text query to search for
-     * @param media_types=["show","movie"] : (string[]|string) : ["show","movie","episode"] A list to filter the search - obtaining only the media types specified.
+     * @param id : string id of the media we want more information about
+     * @param id_type="trakt" : string Type of Id we have
+     * Todo POSSIBLE_ID_TYPES = ["trakt", "TMDB", "tvdb",...]
+     * @param media_types=["show","movie"] : (string[]|string) : ["show","movie","episode"] media type of the id we have.
      * Todo POSSIBLE_MEDIA_TYPES = { SHOW: "show", MOVIE: "movie", EPISODE: "episode"}
      * @param extended=true : boolean whether to get extended results or not.
      */
-    getResultById(token, id, id_type = "trakt", media_types = ["show", "movie"], extended = true) {
-        if (media_types === "") {//
-            media_types = ["show", "movie"];
+    async getResultById(id, id_type = "trakt", media_types = ["show", "movie"], extended = true) {
+        (media_types === "") ? media_types = ["show", "movie"] : '';
+        let params = {type: media_types};
+        (extended) ? params.extended = 'full' : '';
+
+        try {
+            const res = await this.axios({
+                method: 'GET',
+                url: `/search/${id_type}/${id}`, //-> search/trakt/id
+                params
+            });
+            return res.data;
+        } catch (err) {
+            console.error(err.response.status);
+            console.error(err.config);
+            throw this.TraktCallErrorMessage;
         }
-        let searchOptions = {
-            method: 'GET',
-            uri: `${this.APIEndpoint}/search/${id_type}/${encodeURIComponent(id)}?type=${media_types}${extended === true ? `&extended=full` : ''}`, //-> search/type1,type2,type3?..
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-                'trakt-api-version': '2',
-                'trakt-api-key': `${this.clientId}`
-            }, json: true,
-            resolveWithFullResponse: true
-        };
-        return rp(searchOptions);
-
     };
 
-    checkInItem(token, item) {
-        let requestOptions = {
-            method: 'POST',
-            uri: `${this.APIEndpoint}/checkin`,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-                'trakt-api-version': '2',
-                'trakt-api-key': `${this.clientId}`
-            }, json: item,
-            resolveWithFullResponse: true
-        };
-        return rp(requestOptions);
+    async checkInItem(token, item) {
+        try {
+            const res = await this.axios({
+                method: 'POST',
+                url: `/checkin`,
+                data: item,
+                headers: {'Authorization': `Bearer ${token}`}
+            });
+            return res.data;
+        } catch (err) {
+            console.error(err.response.status);
+            console.error(err.config);
+            throw this.TraktCallErrorMessage;
+        }
+        //Todo : Manage checkin error when starting a new checkin 401 when something is missing I think
+        // "If a checkin is already in progress, a 409 HTTP status code will returned. The response will contain an expires_at timestamp which is when the user can check in again."
 
-//Todo : Manage checkin error when starting a new checkin
-// "If a checkin is already in progress, a 409 HTTP status code will returned. The response will contain an expires_at timestamp which is when the user can check in again."
-
+        //Todo : Note, As per https://trakt.tv/branding
+        // Checkin seems to be mobile-oriented while Scrobble is meant to be seamless to the user, being attached to play pause stop events etc, in a media player.
     };
 
-//Todo : Note, As per https://trakt.tv/branding
-// Checkin seems to be mobile-oriented while Scrobble is meant to be seamless to the user, being attached to play pause stop events etc, in a media player.
-
-    deleteCheckins(token) {
-        let requestOptions = {
-            method: 'DELETE',
-            uri: `${this.APIEndpoint}/checkin`,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-                'trakt-api-version': '2',
-                'trakt-api-key': `${this.clientId}`
-            }, json: true,
-            resolveWithFullResponse: true
-        };
-        return rp(requestOptions);
+    async deleteCheckins(token) {
+        try {
+            const res = await this.axios({
+                method: 'DELETE',
+                url: `/checkin`,
+                headers: {'Authorization': `Bearer ${token}`}
+            });
+            return res.data;
+        } catch (err) {
+            console.error(err.response.status);
+            console.error(err.config);
+            throw this.TraktCallErrorMessage;
+        }
     };
 
 
